5bcf90e4b5fef5a1d2ff5958f2d15d381f6f6ec7
Mitchell <94mmitchellz@protonmail.com>
2022-01-25 16:54:20 -0500

    Change welcome message & add welcome channel

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,8 +8,8 @@ module.exports = {
 	execute(member) {
 		let client = member.client
 		const channel = member.guild.channels.cache
-			.find(channel => channel.id === client.customData.channelCodes["main-chat"])
-			.send(`=~=~=~=~=~=~=~=~=~=~=~=~=\n\nHey ${member.user}! Welcome to the discord. Feel free to take a look around and chill with us.\n\n**To get started, pick out all the games your interested in playing with us here: ${client.customData.channels["pick-your-games"]}**\n\nIf you're here for ArcheAge pirating, please message ${client.customData.adminUsers["Lexi"]} to get access to the pirating channels via the Pirate role\n\n=~=~=~=~=~=~=~=~=~=~=~=~=`);
+			.find(channel => channel.id === client.customData.channelCodes["welcome-chat"])
+			.send(`=~=~=~=~=~=~=~=~=~=~=~=~=\n\nHey ${member.user}! Welcome to the discord. Feel free to take a look around and chill with us.\n\n**To get started, pick out all the games your interested in playing with us here: ${client.customData.channels["pick-your-games"]}**\n\nIf you join at 3:00 in the morning, est time there is a chance we are probably asleep.\n\n=~=~=~=~=~=~=~=~=~=~=~=~=`);
 		client.customData.newInviteTimes[member.user.username] = member.joinedAt
 		giveFollower(member)
 	},
